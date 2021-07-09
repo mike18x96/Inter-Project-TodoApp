2680a83a0f10e458e5112baec0a0439c811403b3
ckep <cezary.kepa@inetum.world>
2021-07-09 09:21:59 +0200

    Zadanie 9 - punkt startowy
    (cherry picked from commit 378ae29901fb7c1840acf7a1e443e69d80e4c324)

---
+++ src/main/java/com/inetum/training/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.inetum.training.security.config;//package com.impaqgroup.training.security.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//
+////    @Bean
+////    PasswordEncoder passwordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService);
+//    }
+//
+//}
