446077a43b48bff56e5192d87a6df819c817cf3b
aole1 <artur.olejniczak@inteum.com>
2022-01-19 17:01:22 +0100

    Ex6
    Ad3. Add more REST tests

--- src/test/java/com/inetum/training/todo/controller/StandaloneTodoRestControllerTest.java
+++ src/test/java/com/inetum/training/todo/controller/StandaloneTodoRestControllerTest.java
@@ -1,16 +1,19 @@
 package com.inetum.training.todo.controller;
 
-import com.inetum.training.todo.service.fake.TodoFakeRepositoryImpl;
 import com.inetum.training.todo.domain.Todo;
-import org.hamcrest.Matchers;
+import com.inetum.training.todo.service.fake.TodoFakeRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 
 public class StandaloneTodoRestControllerTest {
@@ -22,66 +25,206 @@ public class StandaloneTodoRestControllerTest {
     public static final String URL = "/todos";
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         fakeRepository = new TodoFakeRepositoryImpl();
-        mockMvc = MockMvcBuilders.standaloneSetup(new TodoRestController(fakeRepository))
-                 .build();
+        mockMvc = standaloneSetup(new TodoRestController(fakeRepository))
+                .build();
     }
 
     @Test
     public void getAll_properRequest_returns200() throws Exception {
         //given
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get(URL);
-        MediaType applicationtJson= new MediaType(MediaType.APPLICATION_JSON.getType(),
-                MediaType.APPLICATION_JSON.getSubtype()
-        );
+        MockHttpServletRequestBuilder mockRequest = get(URL);
         //when
         mockMvc.perform(mockRequest)
-        //then
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.content().contentType(applicationtJson));
+                .andDo(print())
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON));
     }
 
     @Test
     public void getAll_TodoFound_returnsTodo() throws Exception {
         //given
-        Todo todo = new Todo(-1L, "nazwa", "priorytet", "opis", true);
-        Todo todo2 = new Todo(-1L, "nazwa2", "priorytet2", "opis2", true);
+        Todo todo = new Todo(1L, "nazwa", "priorytet", "opis", true);
+        Todo todo2 = new Todo(2L, "nazwa2", "priorytet2", "opis2", true);
         fakeRepository.save(todo);
         fakeRepository.save(todo2);
         //when
-        mockMvc.perform(MockMvcRequestBuilders.get(URL))
-//                .andDo(MockMvcResultHandlers.print())
-        //then
-            .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.is("nazwa")))
-            .andExpect(MockMvcResultMatchers.jsonPath("$[1].name", Matchers.is("nazwa2")));
+        mockMvc.perform(get(URL))
+                .andDo(print())
+                //then
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("nazwa")))
+                .andExpect(jsonPath("$[1].name", is("nazwa2")));
     }
 
     @Test
-    public void getOne_nothingFound_returns404() throws Exception{
+    public void getOne_nothingFound_returns404() throws Exception {
         //when
-        mockMvc.perform(MockMvcRequestBuilders.get(URL + "/1"))
-        //then
-            .andExpect(MockMvcResultMatchers.status().isNotFound());
+        mockMvc.perform(get(URL + "/1"))
+                .andDo(print())
+                //then
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$", is("nie znaleziono elementu o podanym id")));
     }
 
     @Test
-    public void post_elementWithoutName_returns400() throws Exception{
+    public void getOne_TodoFound_returns200() throws Exception {
+        //given
+        Todo todo = new Todo(1L, "nazwa", "priorytet", "opis", true);
+        fakeRepository.save(todo);
+        //when
+        mockMvc.perform(get(URL + "/1")
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
+
+    }
+
+    @Test
+    public void post_elementWithoutName_returns400() throws Exception {
         //given
         String jsonTodo = "{\"priority\":\"wazny\", " +
-                           "\"description\":\"opis\", " +
-                           "\"completed\":\"false\"}";
-        MediaType applicationtJson = new MediaType(MediaType.APPLICATION_JSON.getType(),
-                MediaType.APPLICATION_JSON.getSubtype()
-        );
+                "\"description\":\"opis\", " +
+                "\"completed\":\"false\"}";
         //when
-        mockMvc.perform(MockMvcRequestBuilders.post(URL)
-                    .contentType(applicationtJson)
-                    .content(jsonTodo)
-        //then
-        ).andExpect(MockMvcResultMatchers.status().is4xxClientError());
+        mockMvc.perform(post(URL)
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonTodo)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().is4xxClientError());
 
+    }
+
+    @Test
+    public void post_completeElement_returns201() throws Exception {
+        //given
+        Todo todo = new Todo(1L, "nazwa", "priorytet", "opis", true);
+        Todo todo2 = new Todo(2L, "nazwa2", "priorytet2", "opis2", true);
+        fakeRepository.save(todo);
+        fakeRepository.save(todo2);
+        String jsonTodo = "{\"name\":\"nowa nazwa\", " +
+                "\"priority\":\"wazny\", " +
+                "\"description\":\"opis\", " +
+                "\"completed\":\"false\"}";
+        //when
+        mockMvc.perform(post(URL)
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonTodo)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().isCreated());
+
+    }
+
+    @Test
+    public void put_elementWithoutName_returns400() throws Exception {
+        //given
+        String jsonTodo = "{\"id\":\"2\", " +
+                "\"priority\":\"wazny\", " +
+                "\"description\":\"opis\", " +
+                "\"completed\":\"false\"}";
+        //when
+        mockMvc.perform(put(URL + "/2")
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonTodo)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void put_completeElementWhenTodoExists_returns2xx() throws Exception {
+        //given
+        Todo todo = new Todo(2L, "nazwa1", "priorytet3", "opis1", true);
+        fakeRepository.save(todo);
+        String jsonTodo = "{\"name\":\"nazwa\", " +
+                "\"priority\":\"wazny\", " +
+                "\"description\":\"opis\", " +
+                "\"completed\":\"false\"}";
+        //when
+        mockMvc.perform(put(URL + "/2")
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonTodo)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void put_completeElementWhenTodoNotExists_returns2xx() throws Exception {
+        //given
+        Todo todo = new Todo(1L, "nazwa1", "priorytet3", "opis1", true);
+        fakeRepository.save(todo);
+        String jsonTodo = "{\"id\":\"2\", " +
+                "\"name\":\"nazwa\", " +
+                "\"priority\":\"wazny\", " +
+                "\"description\":\"opis\", " +
+                "\"completed\":\"false\"}";
+        //when
+        mockMvc.perform(put(URL + "/2")
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonTodo)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void put_withoutNameElementWhenTodoNotExists_returns4xx() throws Exception {
+        //given
+        Todo todo = new Todo(1L, "nazwa1", "priorytet3", "opis1", true);
+        fakeRepository.save(todo);
+        String jsonTodo = "{\"id\":\"2\", " +
+                "\"priority\":\"wazny\", " +
+                "\"description\":\"opis\", " +
+                "\"completed\":\"false\"}";
+        //when
+        mockMvc.perform(put(URL + "/2")
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonTodo)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void delete_TodoExists_returns204() throws Exception {
+        //given
+        Todo todo = new Todo(1L, "nazwa1", "priorytet3", "opis1", true);
+        fakeRepository.save(todo);
+        //when
+        mockMvc.perform(delete(URL + "/1")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void delete_TodoNotExists_returns404() throws Exception {
+        //given
+        Todo todo = new Todo(2L, "nazwa1", "priorytet3", "opis1", true);
+        fakeRepository.save(todo);
+        //when
+        mockMvc.perform(delete(URL + "/100")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                //then
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$", is("nie znaleziono elementu o podanym id")));
     }
 
 
