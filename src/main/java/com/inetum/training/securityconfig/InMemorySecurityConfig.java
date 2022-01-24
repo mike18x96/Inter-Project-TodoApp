5a0c1747a8d03e7971f516b56470692856809ec6
aole1 <artur.olejniczak@inteum.com>
2022-01-24 20:00:48 +0100

    Ex8 Ad.4 Basic Security Config
    Change default encoder to PasswordEncoder

--- src/main/java/com/inetum/training/securityconfig/InMemorySecurityConfig.java
+++ src/main/java/com/inetum/training/securityconfig/InMemorySecurityConfig.java
@@ -1,30 +1,37 @@
-//package com.inetum.training.securityconfig;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.httpBasic();
-//        http.authorizeRequests()
-//                .antMatchers("/users").hasRole("ADMIN")
+package com.inetum.training.securityconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class InMemorySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.httpBasic();
+        http.authorizeRequests()
+                .antMatchers("/users").hasRole("ADMIN")
+                .antMatchers("/todos/**","/search/**").hasRole("USER");
+
 //                .antMatchers("/todos").access("hasRole('ROLE_USER')");  //w innym stylu
-//
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("heniek").password("heniek").roles("USER");
-//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-//    }
-//
-//}
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("heniek").password(encoder.encode("heniek")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN","USER");
+    }
+
+
+
+}
