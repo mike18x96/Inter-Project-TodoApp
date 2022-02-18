cc1b960906ec01a3c849b5017be98f46cda2414a
mike18x96 <mike18x96@gmail.com>
2022-02-18 09:36:12 +0100

    ExJWT but not working

--- src/main/java/com/inetum/training/security/config/SecurityConfig.java
+++ src/main/java/com/inetum/training/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.inetum.training.security.config;
 
+import com.inetum.training.jwt.JwtTokenVerifier;
+import com.inetum.training.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,17 +37,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder);
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.httpBasic();
+//        http.headers().frameOptions().disable();
+//        http.authorizeRequests()
+////                .antMatchers("/users").hasRole("ADMIN")
+////                .antMatchers("/todos/**").hasAnyRole("ADMIN", "USER")
+////                .antMatchers("/search/todos/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers("/actuator/info", "/actuator/health").permitAll()
+//                .anyRequest().fullyAuthenticated();
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.httpBasic();
-        http.headers().frameOptions().disable();
-        http.authorizeRequests()
-//                .antMatchers("/users").hasRole("ADMIN")
-//                .antMatchers("/todos/**").hasAnyRole("ADMIN", "USER")
-//                .antMatchers("/search/todos/**").hasAnyRole("ADMIN", "USER")
+        http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig),JwtUsernameAndPasswordAuthenticationFilter.class)
+                .authorizeRequests()
+
                 .antMatchers("/actuator/info", "/actuator/health").permitAll()
-                .anyRequest().fullyAuthenticated();
+                .anyRequest()
+                .authenticated();
     }
 
 }
